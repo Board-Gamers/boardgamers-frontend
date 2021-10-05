ed30792ec471af40e21b0746ebed83e0aa004647
김학영 <gkrdud5698@naver.com>
2021-10-05 15:42:54 +0900

    add initial-rating page

--- src/apis/Intercepter.js
+++ src/apis/Intercepter.js
@@ -5,7 +5,7 @@ const baseUrl = "https://koreanjson.com/users"; // 임시
 axios.interceptors.request.use(
     function(config) {
         // 요청을 보내기 전에 수행할 일
-        console.log("intertcept before request");
+        // console.log("intertcept before request");
         config.headers.Authorization = localStorage.getItem("jwt");
         return config;
     },
@@ -17,12 +17,12 @@ axios.interceptors.request.use(
 axios.interceptors.response.use(
     function(response) {
         // 응답 데이터를 가공
-        console.log("intercept before response");
+        // console.log("intercept before response");
         return response;
     },
     function(error) {
         // 오류 응답을 처리
-        console.log("intercept before error");
+        // console.log("intercept before error");
         return Promise.reject(error);
     }
 );
